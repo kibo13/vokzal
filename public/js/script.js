4dab65739a9ee9a8247918b317a368c2fd9995e4
kibo13 <kimboris1310@gmail.com>
2021-09-01 22:29:08 +0600

    fix: changed redirect to current page when changing localization

--- public/js/script.js
+++ public/js/script.js
@@ -478,3 +478,17 @@ $(document).on("click", "#confirm-order", function (e) {
     });
   }
 });
+
+$(document).on("click", ".bk-lang", function (e) {
+  let hostname = window.location.origin;
+  let locale = this.dataset.locale;
+  let pathname = window.location.pathname.substr(3);
+
+  $.ajax({
+    url: `/${locale}`,
+    method: "GET",
+    success: function (e) {
+      window.location.href = `${hostname}/${locale}${pathname}`;
+    },
+  });
+});
--- resources/views/components/localization.blade.php
+++ resources/views/components/localization.blade.php
@@ -1,11 +1,16 @@
 <div id="locale-toggler" class="header__lang">
-  <span class="active__lang">{{ getCurrentLang() }}</span>
+  <span class="active__lang">
+    @if(getCurrentLang() == 'kk') kz @else {{ getCurrentLang() }} @endif
+  </span>
   <ul id="lang-list" class="header__lang-list d-none">
     @foreach (getArrayLang() as $lang)
       @if(getCurrentLang() !== $lang)
       <li>
-        <a href="{{ route('locale', $lang) }}">
-          {{ $lang }}
+        <a
+          class="bk-lang"
+          {{-- href="{{ route('locale', $lang) }}" --}}
+          data-locale="{{ $lang }}">
+          @if($lang == 'kk') kz @else {{ $lang }} @endif
         </a>
       </li>
       @endif
